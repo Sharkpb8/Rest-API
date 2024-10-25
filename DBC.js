9e974a854cab8e19870966ef86769e9c402cc957
Sharkpb8 <sharkpb8@gmail.com>
2024-10-25 17:44:34 +0200

    working get,post,delete,patch

--- DBC.js
+++ DBC.js
@@ -9,21 +9,47 @@ const pool = mysql.createPool({
     database:  process.env.database
 }).promise()
 
-async function getNodes(){
+export async function getBlogs(){
     const result = await pool.query("select * from blogs")
     const rows = result[0]
     return rows
 }
 
-async function getNode(id){
+export async function getBlog(id){
     const result = await pool.query("select * from blogs where id = ?",[id]) 
     const rows = result[0]
     return rows[0]
 }
 
-async function createNode(autor,text,date){
-    const [result] = await pool.query("insert into blogs (autor,text,date) values(?,?,?)",[autor,text,date])
+export async function createBlog(autor,text,date){
+    let result;
+    if(date != null){
+        [result] = await pool.query("insert into blogs (autor,text,date) values(?,?,?)",[autor,text,date]);
+    }else{
+        [result] = await pool.query("insert into blogs (autor,text) values(?,?)",[autor,text]);
+    }
+    
     return result.insertId
+}
+
+export async function deleteBlog(id){
+    const result = await pool.query("delete from blogs where id = ?",[id])
+    return result
+}
+
+export async function updateBlog(id,autor,text,date){
+    const temp = await getBlog(id)
+    if(autor == null){
+        autor = temp.autor
+    }
+    if(text == null){
+        text = temp.text
+    }
+    if(date == null){
+        date = temp.date
+    }
+    const result = await pool.query("update blogs set autor = ?, text = ?, date = ? where id = ?",[autor,text,date,id])
+    return result
 }
 
 /* const notes = await getNodes()
@@ -32,5 +58,5 @@ console.log(notes) */
 /* const note = await getNode(1)
 console.log(note) */
 
-const result = await createNode("martin","zaspal jsem","2024-10-25")
-console.log(result)
+/* const result = await createNode("martin","zaspal jsem","2024-10-25")    
+console.log(result) */
---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { getBlog,getBlogs,createBlog,deleteBlog,updateBlog } from './DBC.js'
+
+const app = express()
+app.use(express.json())
+
+app.get("/api/blog",async(req,res) => {
+    const blogs = await getBlogs()
+    res.send(blogs)
+})
+
+app.get("/api/blog/:id",async(req,res) => {
+    const id =req.params.id
+    const blogs = await getBlog(id)
+    res.send(blogs)
+})
+
+app.post("/api/blog", async (req,res) =>{
+    const {autor,text,date} = req.body
+    const blogs = await createBlog(autor,text,date)
+    res.status(201).send(blogs)
+})
+
+app.delete("/api/blog/:id",async(req,res) => {
+    const id =req.params.id
+    const blogs = await deleteBlog(id)
+    res.send(blogs)
+})
+
+app.patch("/api/blog/:id",async(req,res) => {
+    const id =req.params.id
+    const {autor,text,date} = req.body
+    const blogs = await updateBlog(id,autor,text,date)
+    res.send(blogs)
+})
+
+app.use((err, req, res, next) =>{
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+})
+
+app.listen(8080,() =>{
+    console.log("Server is running on port 8080")
+})
